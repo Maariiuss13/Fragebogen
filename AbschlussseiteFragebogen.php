9288eba9b625ba5b9ab14872b59bcfb3c8838165
Marius Müller <mm.mueller13@googlemail.com>
2020-04-21 19:28:10 +0200

    Aktuellster Stand

---
+++ AbschlussseiteFragebogen.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Abschlussseite Fragebogen</title>
+  <link href="Abschlussseitedesign.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <div align="center">
+    <table>
+        <tr>
+            <th><img src="Fragen.jpg" width="100%" height="15%"></th>
+        </tr>
+    </table>
+</div>
+
+<div class="container">
+    <h1 align="center"><b>Willkommen auf der Abschlussseite des Fragebogens!</b></h3>
+        
+       
+</div>
+
+
+</body>
+
+</html>
