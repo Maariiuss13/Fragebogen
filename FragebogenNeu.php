2f98e316e418184e9129dd00cff77520db4c167c
Thoebes <dajana.thoebes@kumavision.com>
2020-04-28 22:19:39 +0200

    FragebogenNeu + Insert aktualisiert

--- FragebogenNeu.php
+++ FragebogenNeu.php
@@ -1,89 +1,44 @@
-<!DOCTYPE html>
+
 <?php
 include 'includes/header.php';
 ?>
 
   <h2 align="center">Neuer Fragebogen</h2>
 
-  <form action="http://vorlesungen.kirchbergnet.de/inhalte/DB-PR/output_posted_vars.php" method="post">
-        <div>
-            <label for="titelFragebogen"><b>Titel Fragebogen</b></label>
-            <input type="text" placeholder="Titel" name="titelFragebogen" required>
-            <br/>
-            <label for="beschreibungFB"><b>Beschreibung Fragebogen</b></label>
-            <input type="text" placeholder="Beschreibung" name="beschreibungFB" required>
-            <br/>
-            <!-- Befrager soll raus und durch Session-Variable ersetzt werden  includes/dbInsertFragebogen.php-->
-            <?php
+    <div>
+        <p>Bitte geben Sie einen Titel für Ihren Fragebogen sowie eine Beschreibung und die Anzahl der gewünschten Fragen an. <br/>
+        Beachten Sie, dass der Titel des Fragebogens einmalig sein muss. <br/>
+        Sollte der gewählte Titel bereits vergeben sein, erhalten Sie eine entsprechende Fehlermeldung.
+        </p>
+        
+        <!-- Echo Befrager-->
+        <?php
             $befrager=$_SESSION['session_bname'];
-            echo "<label for='nameBefrager'><b>Name Befrager</b></label>
-                  <p>".$befrager."</p>";
-            ?>
+            echo "<p> Ersteller Fragebogen: ".$befrager."</p><br/>";
+        ?>
+
+        <form action="includes/dbInsertFragebogen.php" method="post">
+            <fieldset>
+            <legend>Neuen Fragebogen erstellen</legend>
+
+            
+                <label for="titelFragebogen"><b>Titel Fragebogen</b></label>
+                <input type="text" placeholder="Titel" name="titelFragebogen" required>
+                <br/>
+
+                <label for="beschreibungFB"><b>Beschreibung Fragebogen</b></label>
+                <input type="text" placeholder="Beschreibung" name="beschreibungFB" required>
+                <br/>
+        
+                <label for="anzahlFragen"><b>Anzahl Fragen</b></label>
+                <input type="number" value="0" name="anzahlFragen" required>
+
+            </fieldset>
             <br/>
-            <label for="anzahlFragen"><b>Anzahl Fragen</b></label>
-            <input type="number" value="0" name="anzahlFragen" required>
-        </div>
-        <br/> <br/> <br/>
-        <div> 
-            <h3>Fragen erfassen</h3>
-            <table>
-                <tr> 
-                    <th>Nr.</th>
-                    <th> Beschreibung </th>
-                </tr>
-                <tr> 
-                    <td> 
-                        <form> 
-                            <input type="text" name="frageNr" size="1" value="1" readonly/>
-                        </form>
-                    </td>
-                    <td>
-                        <form> 
-                            <input type="text" name="beschreibungF" size="50" required/>
-                        </form>
-                    </td>
-                </tr>
-            </table>
-            <br/>
-            <form action="FragebogenNeuKopie.php" method="post"> 
-                <input type="submit" name="addFrage" value="Frage hinzufügen"/>
-            </form>
-        </div>
-        <br/> <br/>
-        <div>
-            <h3>Fragebogen für folgende Kurse freischalten:</h3>
-            <table>
-                <tr> 
-                    <th>Kürzel</th>
-                    <th> Beschreibung </th>
-                </tr>
-                <tr> 
-                    <td> 
-                        <form> 
-                            <select name="kurse" size="1" required>
-                                <option>WWI318</option>
-                                <option>WWI218</option>
-                                <option>WWI118</option>
-                            </select>
-                        </form>
-                    </td>
-                    <td>
-                        <form> 
-                            <input type="text" name="kursBeschreibung" size="50"/>
-                        </form>
-                    </td>
-                </tr>
-            </table>
-            <br/>
-            <form method="post"> 
-                <input type="submit" name="addKurs" value="Kurs hinzufügen"/>
-            </form>
-        </div>
-        <br/> <br/>
-        <div> 
-            <input type="submit" name="speichernFragebogen" value="Bearbeitung beenden und speichern">
-        </div>
-    </form>
+            <input type="submit" name="speichernFragebogen" value="Fragebogen speichern">
+        </form>
+        
+    </div>
 
 
 </body>
--- includes/dbInsertFragebogen.php
+++ includes/dbInsertFragebogen.php
@@ -1,22 +1,42 @@
 <?php
 include 'dbHandler.php';
+session_start();
+
 
 // Deklaration Variablen
-$titel= $_POST["titelFragebogen"];
-$beschreibung= $_POST["beschreibungFB"];
-$beschreibungF= $_POST["beschreibungF"]
-// Befrager noch durch Session Variable ersetzen
-$befrager= $_POST["nameBefrager"];
-
-//Insert Fragebogen
-$sqlFB="INSERT INTO frageboegen (titel, beschreibung, befrager) VALUES ($titel, $beschreibung, $befrager);"; 
-mysqli_query($conn, $sqlFB);
+$titel = $_POST["titelFragebogen"];
+$beschreibung = $_POST["beschreibungFB"];
+$befrager = $_SESSION["session_befrager"];
 
 
-//Insert Fragen
-$sqlF= "INSERT INTO fragen (titel, fragestellung) VALUES ($titel, $beschreibungF);";
-mysqli_query($conn, $sqlF);
+
+if(isset($_POST["speichernFragebogen"])){
+    //Prüfen, ob Felder befüllt 
+    if(empty($titel) || empty($beschreibung)){
+        header("Location: ../FragebogenNeu.php?error=leerefelder");
+    }
+    //Prüfen, ob AnzahlFragen > 0
+    elseif(($_POST["anzahlFragen"]<=0)){
+        header("Location: ../FragebogenNeu.php?error=AnzahlFragenKleinerGleichNull");
+    }
+    //Prüfen, ob DS schon vorhandel fehlt noch
+    else{
+        //Insert Fragebogen
+        //Session-Variable einsetzen???????????
+        $sql= "INSERT INTO frageboegen(titel, beschreibung, befrager) VALUES('$titel', '$beschreibung', 'Dajena');";
+        mysqli_query($conn, $sql);
+    }
+     
 
 
-//Weiterleitung auf Befrager-Seite
-//header("Location: ../Befrager.php?FragebogenSpeichern=erfolgreich");
+}
+
+
+
+//Weiterleitung auf Fragen-Seite
+if (!$sql) {
+    echo mysqli_error();
+}
+else {
+    header("Location: ../Fragenseiten.php?FragebogenSpeichern=erfolgreich");
+}
