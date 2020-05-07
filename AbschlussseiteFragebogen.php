0b7d06d844dbe256daf5ce738cb54a069fb906fb
Marius Müller <mm.mueller13@googlemail.com>
2020-05-07 22:35:04 +0200

    Datenbankverbindung eingerichtet

--- AbschlussseiteFragebogen.php
+++ AbschlussseiteFragebogen.php
@@ -1,5 +1,77 @@
-<?php include 'includes/header.php'
+<?php include 'includes/header.php';
+require 'includes/dbHandler.php';
+
+if (isset($_POST['aktion']) and $_POST['aktion']=='Speichern') {
+  
+  $kommentar = "";
+  if (isset($_POST['kommentar'])) {
+      $kommentar = trim($_POST['kommentar']);
+  }
+  $erstellt = date("Y-m-d H:i:s");
+  if ( $kommentar != '' )
+  {
+      // speichern
+      $einfuegen = $db->prepare("
+              INSERT INTO kontakte (kommentar, erstellt) 
+              VALUES ( ?, NOW())
+              ");
+      $einfuegen->bind_param('s', $kommentar);
+      if ($einfuegen->execute()) {
+          header('Location: ../Startseite.php?aktion=feedbackgespeichert');
+          die();
+          echo "<h1>gespeichert</h1>";
+      }
+  }   
+}
+if (isset($_GET['aktion']) and $_GET['aktion']=='feedbackgespeichert') {
+  echo '<p class="feedbackerfolg">Datensatz wurde gespeichert</p>';
+}
+$daten = array();
+if ($erg = $conn->query("SELECT * FROM bearbeitenfb")) {
+  if ($erg->num_rows) {
+      while($datensatz = $erg->fetch_object()) {
+          $daten[] = $datensatz;
+      }
+      $erg->free();
+  }   
+}
+if (!count($daten)) {
+  echo "<p>Es liegen keine Daten vor :(</p>";
+} else {
 ?>
+  <table>
+      <thead>
+          <tr>
+              <th>Kommentar</th>
+              <th>erstellt</th>
+          </tr>
+      </thead>
+      <tbody>
+          <?php
+          foreach ($daten as $inhalt) {
+          ?>
+              <tr>
+                  
+                  <td><?php echo bereinigen($inhalt->kommentar); ?></td>
+                  <td><?php echo $inhalt->erstellt; ?></td>
+              </tr>
+          <?php
+          }
+          ?>
+      </tbody>
+  </table>
+<?php   
+}
+function bereinigen($inhalt='') {
+  $inhalt = trim($inhalt);
+  $inhalt = htmlentities($inhalt, ENT_QUOTES, "UTF-8");
+  return($inhalt);
+}
+?>
+
+
+
+
   <link href="Abschlussseitedesign.css" rel="stylesheet">
 
 
@@ -13,7 +85,8 @@
   <p>Falls es noch Anmerkungen geben sollte, können Sie diese gerne hier eintragen:</p>
   <label for="text">Anmerkung</label><br/>
       <textarea id="text" name="text" cols="35" rows="4"></textarea><br/> 	
-      <input type="submit" value="Speichern" />
+      <input type="hidden" name="aktion" value="Speichern">
+      <input type="submit" value="Speichern">
 </div>
 
 </body>
--- Auswertungsseite.php
+++ Auswertungsseite.php
@@ -4,8 +4,21 @@ $sql= "SELECT * FROM frageboegen WHERE befrager = 'Marius';";
         $result = mysqli_query($conn, $sql);
         if ($result) {
           $resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
-          print_r($resultArray);
+          //print_r($resultArray);
         } 
+
+$sql= "SELECT * FROM bearbeitenfb WHERE titel = 'Studium';";
+$result = mysqli_query($conn, $sql);
+if ($result) {
+  $resultArray2 = mysqli_fetch_all($result,MYSQLI_ASSOC);
+}
+
+$sql= "SELECT * FROM fragen WHERE titel = 'Studium';";
+$result = mysqli_query($conn, $sql);
+if ($result) {
+  $resultArray3 = mysqli_fetch_all($result,MYSQLI_ASSOC);
+}
+
 ?>
   <link href="Auswertungsdesign.css" rel="stylesheet">
 
@@ -18,31 +31,45 @@ $sql= "SELECT * FROM frageboegen WHERE befrager = 'Marius';";
   <br />
   <div class="main">
     <?php 
-    echo '<p>Name Fragenbogen: '.$resultArray[0]["Titel"].'</p>';
+    echo '<p>Name Fragenbogen: '.$resultArray[1]["Titel"].'</p>';
     echo '<p>Zeitstempel Auswertung: <input type="number" name="mwst" size="2" value="" readonly></p>';
     echo '<p>Anzahl Teilnehmer: <input type="number" name="mwst" size="2" value="" readonly></p>';
     ?>
   </div>
   <br />
   <div class="table">
-    <table border="3">
+    <table border="1">
       <tr>
         <th>Frage</th>
         <th>Durchschnitt</th>
         <th>Min</th>
         <th>Max</th>
         <th>Standardabweichung</th>
-
+      </tr>
+      <tr>
+      <?php
+      echo '<td>'.$resultArray3[0]["Fragestellung"].'</td>';
+      ?>
+      </tr>
     </table>
   </div>
   <br />
   <div class="comments">
     <p>Liste Kommentare</p>
-    <table border="3">
+    <table border="1">
+    
       <tr>
-        <th>Kommentare</th>
+      <?php
+        echo '<th>Kommentare</th>';
+        
 
-
+      
+      echo '</tr>';
+      echo '<tr>';
+      echo '<td>'.$resultArray2[0]["Kommentar"].'</td>';
+      ?>
+   </tr>
+   
     </table>
   </div>
 
