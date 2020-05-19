27d65be496412da82ff10e5ded105f8d01d5a50f
Thoebes <dajana.thoebes@kumavision.com>
2020-05-19 16:54:03 +0200

    DeleteFragen mit preparedstatement

--- includes/dbDeleteFragen.php
+++ includes/dbDeleteFragen.php
@@ -9,11 +9,23 @@ $befrager = $_SESSION["session_bname"];
 
 
 if (isset($_POST["löschenFrage"])) {
-    //Prüfen, ob DS zu Befrager gehört, fehlt!!!!!!!!!!!!
     //Delete Frage
-    $sql = "DELETE FROM Fragen WHERE Fragestellung = '$frage';";
-    mysqli_query($conn, $sql);
-    //Update Fragennr notwendig? !!!!!!!!!!!!!!!!!!
+    $sql = "DELETE FROM Fragen WHERE Fragestellung = ?;";
+    mysqli_stmt_init($conn);
+    // prepared statement erstellt
+    $stmt= mysqli_stmt_init($conn);
+    // prepared statement vorbereiten
+    if (!mysqli_stmt_prepare($stmt, $sql)){
+        header("Location: ../FragenKopieBearbeiten.php?error=SQLBefehlFehler");
+    }
+    else{
+        //Verknüpfung Parameter zu Placeholder
+        mysqli_stmt_bind_param($stmt, "s", $frage);
+        //Parameter in DB verwenden
+        mysqli_stmt_execute($stmt);
+        mysqli_query($conn, $sql);
+        //Update Fragennr notwendig? !!!!!!!!!!!!!!!!!!
+    }
 }
 
 
